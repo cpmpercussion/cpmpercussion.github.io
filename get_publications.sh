9b5f5af2437805824662cb2ee66d844ba70d8653
Charles Martin <charles.martin@anu.edu.au>
2026-06-28 19:32:19 +0800

    updating preprints list

--- get_publications.sh
+++ get_publications.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -q https://github.com/cpmpercussion/cpm-website-publications-list/raw/master/publications.bib -O ./_bibliography/publications.bib
+wget -q https://github.com/cpmpercussion/preprints/raw/refs/heads/main/publications.bib -O ./_bibliography/publications.bib
 sed -i'' -e '/@/s/://g' ./_bibliography/publications.bib
 sed -i'' -e '/Date-Added/d' ./_bibliography/publications.bib
 sed -i'' -e '/Date-Modified/d' ./_bibliography/publications.bib
